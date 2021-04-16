2f88be0dac9a63b11c847d73fc477238185e7eb3
Jake Wood <jakealistairwood@gmail.com>
2021-04-16 15:27:42 +0100

    animated key messages ticker with keyframes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,25 +5,27 @@ import starIcon from '../../assets/img/star.svg';
 const Hero = () => {
 
     const messages = ['Best-In-Class Benefits', 'Climb The Career Ladder', 'A Sunday Times Best Company To Work For', 'Made From Scratch', '#1 Casual Dining Group Brand With Over 10 Restuarants'];
-    
+
     const renderMessages = messages.map(message => {
-        return <li className={styles.message}>
-            <div className={styles.svg__container}>
+        return <li className={styles.ticker__message}>
+            <div className={styles.ticker__messageWrapper}>
                 <img src={starIcon} alt="star-icon" />
+                <span>{message}</span>
             </div>
-            <span>{message}</span>
         </li>
-    })
+    });
 
     return (
         <>
         <header className={styles.hero}>
             <button className={styles.cta__btn}>Find Your Job</button>    
         </header>
-        <section className={styles.keyMessages}>
-            <ul className={styles.messages}>
-                {renderMessages}
-            </ul>
+        <section className={styles.ticker}>
+            <div className={styles.ticker__wrapper}>
+                <ul className={styles.ticker__messages}>
+                    {renderMessages}
+                </ul>
+            </div>
         </section>
         </>
     )
