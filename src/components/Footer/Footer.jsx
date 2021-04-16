793aad090c1605ca85d5aaebe2d0dc7b2e2e1bfe
Jake Wood <jakealistairwood@gmail.com>
2021-04-16 12:21:45 +0100

    social links map working and rendering - footer layout and stylings in situ

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React, { Fragment } from 'react';
+import styles from './Footer.module.scss';
+import facebookIcon from '../../assets/img/facebook.svg';
+import instagramIcon from '../../assets/img/instagram.svg';
+import twitterIcon from '../../assets/img/twitter.svg';
+import linkedinIcon from '../../assets/img/linkedin.svg';
+
+const Footer = () => {
+
+    const socialIcons = [
+        {
+            href: "https://www.facebook.com/lasiguanasuk/",
+            src: `${facebookIcon}`,
+            alt: "facebook-logo"
+        },
+        {
+            href: "https://www.instagram.com/lasiguanas/?hl=en",
+            src: `${instagramIcon}`,
+            alt: "instagram-logo"
+        },
+        {
+            href: "https://twitter.com/lasiguanas",
+            src: `${twitterIcon}`,
+            alt: "twitter-logo"
+        },
+        {
+            href: "https://www.linkedin.com/company/las-iguanas/",
+            src: `${linkedinIcon}`,
+            alt: "linkedin-logo"
+        }
+    ];
+
+    const renderSocialLinks = socialIcons.map(icon => {
+        return <a href={icon.href}>
+            <img src={icon.src} alt={icon.alt} />
+        </a>
+    })
+
+    const copyright = <Fragment>&#169;</Fragment>
+
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.footer__container}>
+                <div className={styles.footer__socialIcons}>
+                    {renderSocialLinks}
+                </div>
+                <p className={styles.copyright}>Privacy Policy {copyright} Las Iguanas 2019</p>
+            </div>    
+        </footer>
+    )
+}
+
+export default Footer;
