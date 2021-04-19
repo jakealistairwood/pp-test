e23bed248305818331359faf299d579221642757
Jake Wood <jakealistairwood@gmail.com>
2021-04-19 10:37:07 +0100

    added in hamburger menu functionality for small tablet and mobile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Navbar.module.scss';
 import logo from '../../assets/img/logo.png';
 
 const Navbar = () => {
+
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const toggleDropdown = isOpen ? styles.menuOpen : '';
+    const toggleLine1 = isOpen ? styles.line__1 : '';
+    const toggleLine2 = isOpen ? styles.line__2 : '';
+    const toggleLine3 = isOpen ? styles.line__3 : '';
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.navbar__logo}>
@@ -11,13 +19,28 @@ const Navbar = () => {
                 </div>
                 <p>People</p>
             </div>    
-            <ul className={styles.navbar__menu}>
-                <li className={styles.navbar__menuItem}>About Us</li>
-                <li className={styles.navbar__menuItem}>News</li>
-                <li className={styles.navbar__menuItem}>Our Roles</li>
-                <li className={styles.navbar__menuItem}>Perks</li>
+            <ul className={`${styles.navbar__menu} ${toggleDropdown}`}>
+                <a href="">
+                    <li className={styles.navbar__menuItem}>About Us</li>
+                </a>
+                <a href="">
+                    <li className={styles.navbar__menuItem}>News</li>
+                </a>
+                <a href="">
+                    <li className={styles.navbar__menuItem}>Our Roles</li>
+                </a>
+                <a href="">
+                    <li className={styles.navbar__menuItem}>Perks</li>
+                </a>
                 <button className={styles.navbar__btn}>Find Your Job</button>
-            </ul>    
+            </ul>
+            <div className={styles.navbar__hamburgerMenu} onClick={() => {
+                setIsOpen(!isOpen)
+            }}>
+                <div className={`${styles.lines} ${toggleLine1}`}></div>
+                <div className={`${styles.lines} ${toggleLine2}`}></div>
+                <div className={`${styles.lines} ${toggleLine3}`}></div>
+            </div>   
         </nav>
     )
 }
